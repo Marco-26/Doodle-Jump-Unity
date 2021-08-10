ba015e289e1fc3a812bde9f354939f7c57c981d6
Marco <markcostah@gmail.com>
2021-08-10 22:25:32 +0100

    feature: add better jump

--- Doodle Jump/Assets/PlayerController.cs
+++ Doodle Jump/Assets/PlayerController.cs
@@ -9,6 +9,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField]private float speed;
     private float jumpForce = 500f;
 
+    // better jump
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
 
     void Start()
     {
@@ -20,20 +23,23 @@ public class PlayerController : MonoBehaviour
         moveInput = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
         Jump();
+        BetterJump();
     }
 
     void Jump()
     {
         if (Input.GetButtonDown("Jump"))
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            rb.velocity = Vector2.up * jumpForce;
         }
     }
 
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    Debug.Log("collision");
-    //    rb.AddForce(Vector2.up * jumpForce);
-    //}
+    void BetterJump()
+    {
+        if(rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+    }
 
 }
