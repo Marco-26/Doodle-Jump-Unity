d670637ed173412b1dd1e8be14ecc68b1a7ef744
Marco <markcostah@gmail.com>
2021-08-11 14:13:56 +0100

    feature : add different types of platform
    - normal platforms
    - big bounce platforms
    - falling platforms

--- Doodle Jump/Assets/Spawner.cs
+++ Doodle Jump/Assets/Spawner.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject platform;
+    public GameObject normalPlatform;
+    public GameObject BigBouncePlatform;
+    public GameObject fallingPlatform;
     private int platformCount = 300;
 
     private void Start()
     {
+
         Vector3 spawnpos = new Vector3();
         for (int i = 0; i < platformCount; i++)
         {
+            var prob = Random.Range(1, 10);
             spawnpos.y += Random.Range(2f, 3f);
             spawnpos.x = Random.Range(-5f, 5f);
-            Instantiate(platform, spawnpos, Quaternion.identity);
+
+            if (prob > 2)
+            {
+                Instantiate(normalPlatform, spawnpos, Quaternion.identity);
+            }
+            else if (prob > 1)
+            {
+                Instantiate(BigBouncePlatform, spawnpos, Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(fallingPlatform, spawnpos, Quaternion.identity);
+            }
         }
     }
 }
