83695adf115b32a4d098710069ba1b674ada92ed
Marco <markcostah@gmail.com>
2021-08-13 23:11:45 +0100

    finish new level generator
    finished new level generator with new probabilities sysytem

--- Doodle Jump/Assets/Spawner.cs
+++ Doodle Jump/Assets/Spawner.cs
@@ -4,57 +4,52 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // public GameObject normalPlatform;
-    // public GameObject BigBouncePlatform;
-    // public GameObject fallingPlatform;
     public Spawn[] platforms;
-    private int platformCount = 300;
+    public int platformCount;
+    private Vector3 spawnpos = new Vector3();
 
-    // private void Start()
-    // {
-    //     Vector3 spawnpos = new Vector3();
-    //     for (int i = 0; i < platformCount; i++)
-    //     {
-    //         var prob = Random.Range(1, 10);
-
-    //         spawnpos.y += Random.Range(2.5f, 2.9f);
-    //         spawnpos.x = Random.Range(-4.5f, 4.5f);
-
-    //         if (prob > 2)
-    //         {
-    //             Instantiate(normalPlatform, spawnpos, Quaternion.identity);
-    //         }
-    //         else if (prob > 1)
-    //         {
-    //             Instantiate(BigBouncePlatform, spawnpos, Quaternion.identity);
-    //         }
-    //         else
-    //         {
-    //             Instantiate(fallingPlatform, spawnpos, Quaternion.identity);
-    //         }
-    //     }
-    // }
-
-
-    void Update()
+    void Start()
     {
-        GenerateLevel();
+        ManageDifficulty();
+        for (int a = 0; a < platformCount; a++)
+        {
+            GenerateLevel();
+        }
+    }
+
+    void ManageDifficulty()
+    {
+        // access spawner scripts and change variables acording to difficulty
+        if (GameValues.difficulty == GameValues.Difficulties.normal)
+        {
+            //do something
+            platformCount = 300;
+        }
+        else if (GameValues.difficulty == GameValues.Difficulties.hard)
+        {
+            //do something
+            platformCount = 600;
+        }
     }
 
     private void GenerateLevel()
     {
+        // generate level based on probabilities
         int n = Random.Range(0, 100);
+
         for (int i = 0; i < platforms.Length; i++)
         {
             if (n >= platforms[i].minProbability && n <= platforms[i].maxProbability)
             {
-                Instantiate(platforms[i].spawnObject, transform.position, Quaternion.identity);
-                break;
+                spawnpos.y += Random.Range(2.5f, 2.9f);
+                spawnpos.x = Random.Range(-5.3f, 5.3f);
+                Instantiate(platforms[i].spawnObject, spawnpos, Quaternion.identity);
             }
         }
     }
 }
 
+// class that contains the platforms to spawn
 [System.Serializable]
 public class Spawn
 {
